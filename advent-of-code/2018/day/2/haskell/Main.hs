49059d8f5c035b3a7c4bd73ab60da5a0e242b55e
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-02 14:32:08 +0000

    day 1

---
+++ advent-of-code/2018/day/2/haskell/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import qualified Data.IntSet   as IntSet
+
+readInt :: String -> Int
+readInt ('+':xs) = read xs
+readInt ('-':xs) = - read xs
+readInt xs = error ("no parse: " ++ xs)
+
+firstRepeat :: [Int] -> Int
+firstRepeat xs = foldr f undefined xs IntSet.empty
+  where
+    f i k ys
+      | IntSet.member i ys = i
+      | otherwise = k (IntSet.insert i ys)
+
+firstFreqRepeat :: [Int] -> Int
+firstFreqRepeat = firstRepeat . scanl (+) 0 . cycle
+
+main :: IO ()
+main = print . firstFreqRepeat . map readInt . lines =<< readFile "../input"
