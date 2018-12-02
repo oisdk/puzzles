98f30600b1cebb280409278b7e4c032f39d41f14
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-02 13:50:36 +0000

    day 1

---
+++ advent-of-code/2018/day/1/haskell/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+readInt :: String -> Int
+readInt ('+':xs) = read xs
+readInt ('-':xs) = - read xs
+
+main :: IO ()
+main = do
+  input <- readFile "../input"
+  print (sum (map readInt (lines input)))
