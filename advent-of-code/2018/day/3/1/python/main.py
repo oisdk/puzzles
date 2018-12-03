ea855dab64eaf89ebdf152faef9807ebbfef77a1
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-03 22:50:42 +0000

    day 3 done

--- advent-of-code/2018/day/3/1/python/main.py
+++ advent-of-code/2018/day/3/1/python/main.py
@@ -1,21 +1,22 @@
 matrix = [ [ 2 for _ in range(1000) ] for _ in range(1000) ]
 
+def parse_coord(line):
+    nm, rest = line.split(' @ ')
+    tx, rest = rest.split(',')
+    ty, rest = rest.split(': ')
+    bx, by = rest.split('x')
+    return (int(nm[1:]), int(tx), int(ty), int(bx), int(by))
+
 with open('../../input') as inp:
     for line in inp:
-        try:
-            _ , rest = line.split('@ ')
-            tx, rest = rest.split(',')
-            ty, rest = rest.split(': ')
-            bx, by = rest.split('x')
-            for x in range(int(tx), int(tx)+int(bx)):
-                for y in range(int(ty), int(ty)+int(by)):
-                    matrix[x][y] -= 1
-        except ValueError:
-            pass
+        _ , tx, ty, bx, by = parse_coord(line)
+        for x in range(tx, tx+bx):
+            for y in range(ty, ty+by):
+                matrix[x][y] -= 1
 
 res = 0
 for row in matrix:
     for cell in row:
-        if cell < 0:
+        if cell <= 0:
             res += 1
 print(res)
---
+++ advent-of-code/2018/day/3/2/python/main.py
@@ -0,0 +1,21 @@
+matrix = [ [ 2 for _ in range(1000) ] for _ in range(1000) ]
+
+def parse_coord(line):
+    nm, rest = line.split(' @ ')
+    tx, rest = rest.split(',')
+    ty, rest = rest.split(': ')
+    bx, by = rest.split('x')
+    return (int(nm[1:]), int(tx), int(ty), int(bx), int(by))
+
+coords = list(map(parse_coord, open('../../input')))
+
+for _ , tx, ty, bx, by in coords:
+    for x in range(tx, tx+bx):
+        for y in range(ty, ty+by):
+            matrix[x][y] -= 1
+
+for nm, tx, ty, bx, by in coords:
+    no_overlap = all(matrix[x][y] == 1 for x in range(tx, tx+bx) for y in range(ty, ty+by))
+    if no_overlap:
+        print(nm)
+        break
