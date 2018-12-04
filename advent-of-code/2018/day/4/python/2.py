e6f2825ec119de3f9b0a2382a971af3cd735cce2
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-04 16:59:16 +0000

    finished day 4

---
+++ advent-of-code/2018/day/4/python/2.py
@@ -0,0 +1,16 @@
+from parse_input import schedules
+from operator import *
+
+freq_sleeps = {
+    guard_id: max(((minute, sum(1 for _, start, end in schedule
+                                if start <= minute < end))
+                   for minute in range(0, 60)),
+                  key=itemgetter(1))
+    for guard_id, schedule in schedules.items()
+}
+
+most_asleep_id, (most_asleep_minute, _) = max(
+    freq_sleeps.items(),
+    key=lambda kv: kv[1][1])
+
+print(most_asleep_minute * most_asleep_id)
