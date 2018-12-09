80a70bc3f67541962052e9e80ce96536a8bc9817
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-09 16:10:55 +0000

    pattern

--- advent-of-code/2018/day/9/python/main.py
+++ advent-of-code/2018/day/9/python/main.py
@@ -2,6 +2,7 @@ with open('../input') as inp:
     line = next(inp).split()
     num_players, final_points = int(line[0]), int(line[6])
 
+
 from itertools import *
 
 marbles = [0]
@@ -10,10 +11,13 @@ position = 0
 
 for player, marble in zip(cycle(range(len(players))), range(1, final_points + 1)):
     if marble >= 23 and marble % 23 == 0:
-        players[player] += marble
         position = (position - 7) % len(marbles)
         players[player] += marbles.pop(position)
     else:
         position = (position + 2) % len(marbles)
         marbles.insert(position, marble)
+
+for marble in range(23, final_points + 1, 23):
+    players[marble % num_players - 1] += marble
+
 print(max(players))
