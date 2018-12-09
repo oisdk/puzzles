f22fb50b377b9bc7a11aa73814a11f1355ce43f6
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-09 16:31:39 +0000

    finished day 9

--- advent-of-code/2018/day/9/python/main.py
+++ advent-of-code/2018/day/9/python/main.py
@@ -1,23 +1,23 @@
+def max_points(num_players, final_points):
+    from itertools import cycle
+    from collections import deque
+
+    marbles = deque([0])
+    players = [0 for _ in range(num_players)]
+
+    for player, marble in zip(cycle(range(len(players))), range(1, final_points + 1)):
+        if marble % 23 == 0:
+            marbles.rotate(7)
+            players[player] += marble + marbles.pop()
+            marbles.rotate(-1)
+        else:
+            marbles.rotate(-1)
+            marbles.append(marble)
+
+    return max(players)
+
 with open('../input') as inp:
     line = next(inp).split()
     num_players, final_points = int(line[0]), int(line[6])
-
-
-from itertools import *
-
-marbles = [0]
-players = [0 for _ in range(num_players)]
-position = 0
-
-for player, marble in zip(cycle(range(len(players))), range(1, final_points + 1)):
-    if marble >= 23 and marble % 23 == 0:
-        position = (position - 7) % len(marbles)
-        players[player] += marbles.pop(position)
-    else:
-        position = (position + 2) % len(marbles)
-        marbles.insert(position, marble)
-
-for marble in range(23, final_points + 1, 23):
-    players[marble % num_players - 1] += marble
-
-print(max(players))
+    print(max_points(num_players, final_points))
+    print(max_points(num_players, final_points * 100))
