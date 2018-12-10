2c472c368cc77896bc17e0d7ec4c234269cb1c00
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-10 17:21:21 +0000

    ncurses

--- advent-of-code/2018/day/10/python/main.py
+++ advent-of-code/2018/day/10/python/main.py
@@ -1,3 +1,10 @@
+import sys
+import time
+secs = int(sys.argv[1])
+lower_bound = int(sys.argv[2])
+upper_bound = int(sys.argv[3])
+steps = upper_bound - lower_bound
+
 from collections import namedtuple
 
 class Point(namedtuple('Point', ['pos_x', 'pos_y', 'vel_x', 'vel_y'])):
@@ -11,27 +18,57 @@ with open('../input') as inp:
         for line in inp
     ]
 
-def print_map(points):
-    min_x = min(p.pos_x for p in points)
-    min_y = min(p.pos_y for p in points)
-    max_x = max(p.pos_x for p in points)
-    max_y = max(p.pos_y for p in points)
-    board = [[' ' for _ in range(201)] for _ in range(21)]
-    for p in points:
-        pos_y = int(20 * ((p.pos_y - min_y) / (max_y - min_y)))
-        pos_x = int(200 * ((p.pos_x - min_x) / (max_x - min_x)))
-        board[pos_y][pos_x] = '●'
-    for row in board:
-        print(''.join(row))
+import curses
 
-import time
-import sys
-steps = int(sys.argv[1])
-lower_bound = int(sys.argv[2])
-upper_bound = int(sys.argv[3])
-for i in range(steps):
-    t = int(lower_bound + ((upper_bound - lower_bound) * (i / steps)))
-    tstr = str(t)
-    print(tstr, '━' * (200 - len(tstr)) )
-    print_map([ p.step(t) for p in points ])
-    time.sleep(3 / float(steps))
+try:
+    screen = curses.initscr()
+    curses.noecho()           # Don't print pressed keys
+    curses.cbreak()           # Don't wait for enter
+    curses.curs_set(False)    # Hide cursor
+    screen.nodelay(True)
+    screen.keypad(True)
+
+    def print_map(points):
+        min_x = min(p.pos_x for p in points)
+        min_y = min(p.pos_y for p in points)
+        max_x = max(p.pos_x for p in points)
+        max_y = max(p.pos_y for p in points)
+        height,width = screen.getmaxyx()
+        for p in points:
+            pos_y = int((height-3) * ((p.pos_y - min_y) / (max_y - min_y)))
+            pos_x = int((width-1) * ((p.pos_x - min_x) / (max_x - min_x)))
+            screen.addstr(pos_y+1, pos_x, "●")
+
+    i = 0
+    def next_step(i):
+        t = int(lower_bound + ((upper_bound - lower_bound) * (i / steps)))
+        tstr = str(t)
+        screen.clear()
+        screen.addstr(0, 0, tstr + '\n')
+        print_map([ p.step(t) for p in points ])
+        screen.refresh()
+
+    while i < steps:
+        if screen.getch() == 32:
+            screen.nodelay(False)
+            while True:
+                c = screen.getch()
+                if c == 32:
+                    break
+                elif c == curses.KEY_LEFT:
+                    i -= 1
+                elif c == curses.KEY_RIGHT:
+                    i += 1
+                next_step(i)
+            screen.nodelay(True)
+        next_step(i)
+        i += 1
+        curses.napms(int((secs * 1000) / steps))
+
+finally:
+    curses.nocbreak()     # Wait for enter
+    curses.echo()         # Print all keyboard input
+    curses.curs_set(True) # Show cursor
+    screen.nodelay(False)
+    screen.keypad(False)
+    curses.endwin()       # Return to normal prompt
