e77dede55f166dc5ee995feb8402857aeb9e1205
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-12-10 17:35:18 +0000

    cleaned

--- advent-of-code/2018/day/10/python/main.py
+++ advent-of-code/2018/day/10/python/main.py
@@ -7,14 +7,18 @@ steps = upper_bound - lower_bound
 
 from collections import namedtuple
 
+
 class Point(namedtuple('Point', ['pos_x', 'pos_y', 'vel_x', 'vel_y'])):
     def step(self, fac=1):
-        return Point(self.pos_x + self.vel_x * fac, self.pos_y + self.vel_y * fac, self.vel_x, self.vel_y)
+        return Point(self.pos_x + self.vel_x * fac,
+                     self.pos_y + self.vel_y * fac, self.vel_x, self.vel_y)
+
 
 with open('../input') as inp:
     points = [
         Point(
-            int(line[10:16]), int(line[18:24]), int(line[36:38]), int(line[40:42]))
+            int(line[10:16]),
+            int(line[18:24]), int(line[36:38]), int(line[40:42]))
         for line in inp
     ]
 
@@ -22,9 +26,9 @@ import curses
 
 try:
     screen = curses.initscr()
-    curses.noecho()           # Don't print pressed keys
-    curses.cbreak()           # Don't wait for enter
-    curses.curs_set(False)    # Hide cursor
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(False)
     screen.nodelay(True)
     screen.keypad(True)
 
@@ -33,19 +37,20 @@ try:
         min_y = min(p.pos_y for p in points)
         max_x = max(p.pos_x for p in points)
         max_y = max(p.pos_y for p in points)
-        height,width = screen.getmaxyx()
+        height, width = screen.getmaxyx()
         for p in points:
-            pos_y = int((height-3) * ((p.pos_y - min_y) / (max_y - min_y)))
-            pos_x = int((width-1) * ((p.pos_x - min_x) / (max_x - min_x)))
-            screen.addstr(pos_y+1, pos_x, "●")
+            pos_y = int((height - 3) * ((p.pos_y - min_y) / (max_y - min_y)))
+            pos_x = int((width - 1) * ((p.pos_x - min_x) / (max_x - min_x)))
+            screen.addstr(pos_y + 1, pos_x, "●")
 
     i = 0
+
     def next_step(i):
         t = int(lower_bound + ((upper_bound - lower_bound) * (i / steps)))
         tstr = str(t)
         screen.clear()
         screen.addstr(0, 0, tstr + '\n')
-        print_map([ p.step(t) for p in points ])
+        print_map([p.step(t) for p in points])
         screen.refresh()
 
     while i < steps:
@@ -66,9 +71,9 @@ try:
         curses.napms(int((secs * 1000) / steps))
 
 finally:
-    curses.nocbreak()     # Wait for enter
-    curses.echo()         # Print all keyboard input
-    curses.curs_set(True) # Show cursor
+    curses.nocbreak()
+    curses.echo()
+    curses.curs_set(True)
     screen.nodelay(False)
     screen.keypad(False)
-    curses.endwin()       # Return to normal prompt
+    curses.endwin()
